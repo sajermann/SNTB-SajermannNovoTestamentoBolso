9c93be7172065ef1dccb32980093de1c965d8d70
Bruno Sajermann <contato@sajermann.com>
2021-06-10 21:32:12 -0300

    feat: Added Reading Screen in Mobile

--- Mobile/src/utils/Verify.ts
+++ Mobile/src/utils/Verify.ts
@@ -3,7 +3,7 @@ import Biblia from "../models/Biblia";
 export const verifyVerse = (item: string, verse: string ): boolean => {
   const numberVerse = [];
 
-    console.log('Tipo do verse: ', typeof(verse), verse);
+    //console.log('Tipo do verse: ', typeof(verse), verse);
     //console.log('Tipo do verse: ', verse);
   for(let i = 0; i < item.length; i++){
     if(!isNaN(parseInt(item.split('')[i]))){
